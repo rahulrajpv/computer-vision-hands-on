670262bb92f885913b7be008d4fc11c860511013
RAHULRAJ P V <113903549+rahulrajpv@users.noreply.github.com>
2023-03-25 02:09:36 +0530

    Corrected errors

--- Q3/01 - Edge based Image segmentation.py
+++ Q3/01 - Edge based Image segmentation.py
@@ -2,10 +2,10 @@
 import numpy as np 
 import cv2 as cv2
 # Read the input image
-img = cv2.imread(r"D:\MTECH\PROJECT\CV\ASSIGNMENT\REPOSITORY\computer-vision-hands-on\Q5\dataset\Sample03.jpg")
+img = cv2.imread(r"D:\MTECH\PROJECT\CV\ASSIGNMENT\REPOSITORY\computer-vision-hands-on\Q5\dataset\Sample03.jpg") # for image read , r used for raw string that is to avoid escape sequence
 
 # Convert the image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # BGR to GRAY conversion
 
 # Apply the Canny edge detector
 edges = cv2.Canny(gray, 100, 200) # 100 and 200 are the thresholds for the hysteresis procedure and gray is the input image
