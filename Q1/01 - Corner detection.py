30d60103fc9884bb8412f5ec3d6dd675373d2d73
RAHULRAJ P V <rahulraj.cs22@duk.ac.in>
2023-03-24 21:47:07 +0530

    01 -  Edge Detection.py

--- Q1/01 - Corner detection.py
+++ Q1/01 - Corner detection.py
@@ -1,26 +1,17 @@
-#importing libraries
+# Importing Libraries
+import numpy as np # for matrix operations
+import cv2 as cv2 # for image processing
 
+# Reading the sample image from the local drive
+image = cv2.imread(r"C:\Users\rahul\OneDrive\Pictures\118644924_610960472901025_1658440095779597941_n.jpg", 0) # reading the image from the local drive and converting it to grayscale image by passing 0 as the second argument
 
-import cv2
+# Resizing the image
+image01 = cv2.resize(image, (500, 500)) # resizing the image to 500x500
 
-# Load the image in grayscale
-img = cv2.imread("C:\Users\rahul\OneDrive\Pictures\sample01.jpg.jpg", 0) #
+# Applying Canny edge detection
+edges = cv2.Canny(image01, 80, 100) # threshold1=80, threshold2=100
 
-# parameters needed for corner detetion 
-max_corners = 100000000 # maximum number of corners to detect
-quality_level = 0.001 # minimum quality of corner
-min_distance = 1 # minimum euclidean distance between corners
-
-# Apply Shi-Tomasi corner detection
-corners = cv2.goodFeaturesToTrack(img, max_corners, quality_level, min_distance) # detect corners
-
-# Convert corners to integers
-corners = corners.astype(int) # convert to integers
-
-# Draw circles at the corners
-for corner in corners: # for each corner
-    x, y = corner.ravel() # get the x and y coordinates
-    cv2.circle(img, (x, y), 1, (255, 0, 0), -1) # draw a circle at the corner
-
-# Display the corners in the image
-cv2_imshow(img) #
+# Displaying the image
+cv2.imshow("Edges", edges) # displaying the image
+cv2.waitKey(0) # waiting for the user to press any key
+cv2.destroyAllWindows() # destroying all the windows
