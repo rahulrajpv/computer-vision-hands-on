96aff4536b185f55ae8d46fbf1376920e31a7419
RAHULRAJ P V <rahulraj.cs22@duk.ac.in>
2023-04-03 23:58:43 +0530

    02 - Region Based Image segmentation.py

--- Q4/02 - Region Based Image segmentation.py
+++ Q4/02 - Region Based Image segmentation.py
@@ -9,12 +9,6 @@ img = cv2.imread(r"D:\MTECH\PROJECT\CV\ASSIGNMENT\REPOSITORY\computer-vision-han
 # Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Apply the Canny edge detector
-edges = cv2.Canny(gray, 100, 200) # 100 and 200 are the thresholds for the hysteresis procedure and gray is the input image
-
-# Display the resulting image
-cv2.imshow("edges",edges) # for image display
-
 # Apply binary thresholding
 _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY) #gray is the image to be thresholded, 150 is the threshold value, 255 is the max value, cv2.THRESH_BINARY is the thresholding type
 
