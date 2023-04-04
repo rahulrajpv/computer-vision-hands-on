3b1d5a1e25969b6fe46f66cf58b31a483ac7dc86
RAHULRAJ P V <rahulraj.cs22@duk.ac.in>
2023-04-04 17:32:23 +0530

    04 - Gabor Filter.py

--- Q2/04 - Gabor Filter.py
+++ Q2/04 - Gabor Filter.py
@@ -1,28 +1,30 @@
-import pywt
-import numpy as np
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Load the image
-img = cv2.imread(r"C:\Users\rahul\OneDrive\Pictures\sample03.jpg", 0)
-
-# Resize the image to a new width and height
-width = 500 # new width in pixels
-height = 300 # new height in pixels
-resized_img = cv2.resize(img, (width, height)) # resize the image
+img = cv2.imread('/content/sample01.jpg', cv2.IMREAD_GRAYSCALE)
 
 
-# Perform the DWT using the 'haar' wavelet
-coeffs = pywt.dwt2(img, 'haar') # returns a tuple of 4 arrays (cA, (cH, cV, cD))
+# Define the Gabor filter parameters
+ksize = 31  # kernel size
+sigma = 5  # standard deviation of Gaussian envelope
+gamma = 0.5  # spatial aspect ratio
+phi_arr = [0, 0, 90, 150]  # orientation of the normal to the parallel stripes of a Gabor function
+lambda_arr = [20, 80, 30, 10]  # wavelength of the sinusoidal factor
+# Create the figure with a larger size
+fig, axs = plt.subplots(nrows=2, ncols=4, figsize=(16, 8))
+# Create the Gabor filter kernel for each parameter combination and apply the filter to the image
+for i in range(len(phi_arr)):
+    theta = phi_arr[i] * np.pi / 180
+    lambd = lambda_arr[i]
+    kernel = cv2.getGaborKernel((ksize, ksize), sigma, theta, lambd, gamma, 0, ktype=cv2.CV_32F)
+    filtered = cv2.filter2D(img, cv2.CV_8UC3, kernel)
 
-# Separate the coefficients into approximation and detail coefficients
-cA, (cH, cV, cD) = coeffs # cA is the approximation coefficients, cH, cV, cD are the detail coefficients
+    # Display the original and filtered images
+    plt.subplot(2, 4, i+1), plt.imshow(img, cmap='gray'), plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 4, i+5), plt.imshow(filtered, cmap='gray'), plt.title(f'Gabor Filtered\nPhi: {phi_arr[i]}, Lambda: {lambda_arr[i]}')
+    plt.xticks([]), plt.yticks([])
 
-# Display the original image and the DWT coefficients
-cv2.imshow('Original Image', img) # original image
-cv2.imshow('Approximation Coefficients', np.uint8(cA)) # approximation coefficients
-cv2.imshow('Horizontal Detail Coefficients', np.uint8(cH)) # detail coefficients
-cv2.imshow('Vertical Detail Coefficients', np.uint8(cV)) # detail coefficients
-cv2.imshow('Diagonal Detail Coefficients', np.uint8(cD)) # detail coefficients 
-cv2.waitKey(0) # wait for a key press
-cv2.destroyAllWindows() # close all windows
-
+plt.show()
